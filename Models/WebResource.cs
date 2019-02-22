4d1989bee8a712c97be5ca39908fd7df5a3d9380
rswafford <rswafford@gmail.com>
2019-02-22 10:05:42 -0600

    Update WebResource.cs

--- Models/WebResource.cs
+++ Models/WebResource.cs
@@ -30,7 +30,6 @@ namespace WebResourceManager.Models
                 {
                     var parts = value.Split('_');
                     CustomizationPrefix = parts[0];
-                    //RemoteName = value.Replace($"{parts[0]}_", string.Empty);
                     string pattern = $"^{parts[0]}_";
                     RemoteName = Regex.Replace(value, pattern, string.Empty);
                 }
